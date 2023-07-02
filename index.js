630e76c133ad6773a717aa2b49e58216b38d1d3d
Anuska352 <anuskachattopadhyay561@gmail.com>
2023-07-02 16:59:18 +0530

    Almost Complete

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev"
   },
 });
+
+const loginsec = document.querySelector('.login-section')
+const registerlink = document.querySelector('.register-link')
+const loginlink = document.querySelector('.login-link')
+
+loginlink.addEventListener('click', () => {
+    loginsec.classList.add('active')
+})
+registerlink.addEventListener('click', () => {
+    loginsec.classList.remove('active')
+})
